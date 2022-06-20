a21a7be1736378160191c5fbfd25fb3ac5672c32
Ronan Doherty <donanroherty@gmail.com>
2022-06-20 12:57:34 +0100

    Add FPS counter

--- gui/src/components/App.jsx
+++ gui/src/components/App.jsx
@@ -1,6 +1,7 @@
 import createBoidsApp from "../../../boids/app"
 import { onMount, createSignal } from "solid-js"
 import Controls from "./Controls"
+import FPSCounter from "./FpsCounter"
 
 function App() {
   let canvas
@@ -13,13 +14,16 @@ function App() {
   })
 
   return (
-    <div class="w-[640px] shadow-sm">
+    <div class="w-[640px] shadow-sm relative">
       <canvas
         ref={canvas}
         class="w-full border-[1px] border-black border-solid rounded-bl-md rounded-br-md"
         width={canvasSize().x}
         height={canvasSize().y}
       ></canvas>
+      <div class="absolute top-0 right-0 z-10 mr-2 mt-2">
+        {boidsApp() && <FPSCounter valueGetter={boidsApp().tick.getLastRealDelta} />}
+      </div>
       <Controls boidsApp={boidsApp()} />
     </div>
   )
